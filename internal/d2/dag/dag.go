8a3107f662325519a8e1e568d78b60fccb194131
vimcki <0xvimcki@gmail.com>
2023-10-16 17:39:41 +0200

    render dags instead of trees

---
+++ internal/d2/dag/dag.go
@@ -0,0 +1,89 @@
+package dag
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"strings"
+)
+
+type dependency struct {
+	ID    int
+	Name  string       `json:"name,omitempty"`
+	Deps  []dependency `json:"deps,omitempty"`
+	Value interface{}  `json:"value,omitempty"`
+}
+
+func Render(graph string) ([]byte, error) {
+	var root dependency
+	if err := json.Unmarshal([]byte(graph), &root); err != nil {
+		return nil, fmt.Errorf("failed to unmarshal graph: %w", err)
+	}
+
+	rootID := 1
+
+	fillIDs(&root, &rootID, make(map[string]int))
+
+	var result []string
+
+	log.Printf("Root: %+v", root)
+
+	generateGraphD2(root, make(map[int]bool), &result)
+
+	filterDuplicates(&result)
+
+	return []byte(strings.Join(result, "\n")), nil
+}
+
+func fillIDs(dep *dependency, id *int, nameToID map[string]int) {
+	oldID, ok := nameToID[dep.Name]
+	if ok {
+		dep.ID = oldID
+	} else {
+		dep.ID = *id
+		nameToID[dep.Name] = *id
+		*id++
+	}
+	for i := range dep.Deps {
+		fillIDs(&dep.Deps[i], id, nameToID)
+	}
+}
+
+func generateGraphD2(dep dependency, nodes map[int]bool, result *[]string) {
+	if dep.ID == 0 {
+		log.Println("Skipping empty node ", dep.Name)
+		return
+	}
+	if !nodes[dep.ID] {
+		name := dep.Name
+		if name == "" {
+			name = "aggregate"
+		}
+		fixedName := strings.ReplaceAll(name, "\"", "\\\"")
+		if dep.Value != nil {
+			rawVal := fmt.Sprintf("%v", dep.Value)
+			val := strings.ReplaceAll(rawVal, "\"", "\\\"")
+			*result = append(*result, fmt.Sprintf("%d: \"%v\\n%s\"\n", dep.ID, val, fixedName))
+		} else {
+			*result = append(*result, fmt.Sprintf("%d: \"%s\"\n", dep.ID, fixedName))
+		}
+		nodes[dep.ID] = true
+	}
+	// Recursively traverse the dependencies and print the edges
+	for _, childDep := range dep.Deps {
+		generateGraphD2(childDep, nodes, result)
+		*result = append(*result, fmt.Sprintf("%d -> %d", dep.ID, childDep.ID))
+	}
+}
+
+func filterDuplicates(result *[]string) {
+	seen := make(map[string]bool)
+	var filtered []string
+	for _, line := range *result {
+		if !seen[line] {
+			seen[line] = true
+			filtered = append(filtered, line)
+		}
+	}
+	*result = filtered
+}
